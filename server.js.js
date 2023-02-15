b8c5ae00cbd5b9131808d777ca2570d3903bad34
Dyego-Barros <gabutdd@gmail.com>
2023-02-15 17:56:41 -0300

    WebScraping Node

---
+++ server.js.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000
+const instatouch = require('instatouch');
+
+//soonasolution
+
+
+app.listen(port, (error)=>{
+    if(error){console.log(error);}else{console.log('listening on port' + port)}
+})
+
+app.get('/',async (req,res)=>{
+    const options = { count: 5, mediaType:['image','video'], download:false}
+    let user =  await instatouch.user('soonasolution', options)
+    //console.log(user);
+    res.send(user);
+})
+
