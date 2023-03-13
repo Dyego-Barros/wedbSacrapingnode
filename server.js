9882803e40ac14c9596f931747062d990ebe80dc
Dyego-Barros <gabutdd@gmail.com>
2023-03-13 15:11:57 -0300

    Atualização Final

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const port = 3000
 const instatouch = require('instatouch');
 
 
-
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.listen(port, (error)=>{
     if(error){console.log(error);}else{console.log('listening on port' + port)}
 })
 
-app.get('/',async (req,res)=>{
-    const options = { count: 5, mediaType:['image','video'], download:false}
-    let user =  await instatouch.user('', options)   
+app.get('/insta/:client/:count',async (req,res)=>{
+    const client = req.params.client;
+    const count = req.params.count;
+    const options = { count: `${count}`, mediaType:['image','video'], download:false}
+   try {
+    let user =  await instatouch.user(`${client}`, options)   
     res.send(user);
+    
+   } catch (error) {
+     res.json(`Error: ${error.message}`);
+   }
 })
 
